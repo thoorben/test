e2e71d053f16f6639604103138aca9ee1b01a5f8
Thorben Bork <thorben.bork.92@gmail.com>
2019-08-30 18:26:01 +0200

    fixed

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import  Api 
 from flask_jwt import JWT
@@ -8,16 +9,14 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:////data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 app.secret_key = "json"
 api = Api(app)
 jwt = JWT(app, authenticate, identity) #/auth
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 
 api.add_resource(Item, "/item/<string:name>")
